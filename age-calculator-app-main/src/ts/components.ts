ad4557474e86e8fbd081153c11a0b450f867b763
Tomek <tomek.wojak796@gmail.com>
2025-11-27 16:08:59 +0100

    handled input states

--- age-calculator-app-main/src/ts/components.ts
+++ age-calculator-app-main/src/ts/components.ts
@@ -12,18 +12,18 @@ const renderCalculatorHead = () => {
 	]);
 	calculatorHead.innerHTML = `
     <div class="w-full md:w-[22%] group"><span class="font-bold text-sm tracking-[0.2rem] text-grey-500 group-[.error]:text-red-400">DAY</span>
-    <input type="number" placeholder="DD" class="py-3 px-2.5 rounded-lg font-bold uppercase text-black text-lg md:text-[1.3rem] placeholder:text-grey-500 w-full border border-[hsla(0,1%,44%,50%)] focus:border-purple-500 focus:outline-0 group-[.error]:border-red-400">
-    <span class="hidden italic text-red-400 font-medium text-xs md:text-sm">Must be a valid day</span></div>
+    <input type="number" placeholder="DD" class="py-3 px-4 rounded-lg font-bold uppercase text-black text-lg md:text-[1.3rem] placeholder:text-grey-500 w-full border border-[hsla(0,1%,44%,50%)] focus:border-purple-500 focus:outline-0 group-[.error]:border-red-400 caret-purple-500 cursor-pointer hover:border-purple-500 transition-colors">
+    <span class="error-txt hidden italic text-red-400 font-medium text-xs md:text-sm"></span></div>
 
     <div class="w-full md:w-[22%] group"><span class="font-bold text-sm tracking-[0.2rem] text-grey-500 group-[.error]:text-red-400">MONTH</span>
-    <input type="number" placeholder="MM" class="py-3 px-2.5 rounded-lg font-bold uppercase text-black text-lg md:text-[1.3rem] placeholder:text-grey-500 w-full border border-[hsla(0,1%,44%,50%)] focus:border-purple-500 focus:outline-0 group-[.error]:border-red-400">
-    <span class="hidden italic text-red-400 font-medium text-xs md:text-sm">Must be a valid day</span></div>
+    <input type="number" placeholder="MM" class="py-3 px-4 rounded-lg font-bold uppercase text-black text-lg md:text-[1.3rem] placeholder:text-grey-500 w-full border border-[hsla(0,1%,44%,50%)] focus:border-purple-500 focus:outline-0 group-[.error]:border-red-400 caret-purple-500 cursor-pointer hover:border-purple-500 transition-colors">
+    <span class="error-txt hidden italic text-red-400 font-medium text-xs md:text-sm"></span></div>
 
     <div class="w-full md:w-[22%] group"><span class="font-bold text-sm tracking-[0.2rem] text-grey-500 group-[.error]:text-red-400">YEAR</span>
-    <input type="number" placeholder="YYYY" class="py-3 px-2.5 rounded-lg font-bold uppercase text-black text-lg md:text-[1.3rem] placeholder:text-grey-500 w-full border border-[hsla(0,1%,44%,50%)] focus:border-purple-500 focus:outline-0 group-[.error]:border-red-400">
-    <span class="hidden italic text-red-400 font-medium text-xs md:text-sm">Must be a valid day</span></div>
+    <input type="number" placeholder="YYYY" class="py-3 px-4 rounded-lg font-bold uppercase text-black text-lg md:text-[1.3rem] placeholder:text-grey-500 w-full border border-[hsla(0,1%,44%,50%)] focus:border-purple-500 focus:outline-0 group-[.error]:border-red-400 caret-purple-500 cursor-pointer hover:border-purple-500 transition-colors">
+    <span class="error-txt hidden italic text-red-400 font-medium text-xs md:text-sm"></span></div>
 
-    <button class="absolute grid place-items-center w-16 h-16 left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 md:left-[unset] md:translate-x-0 md:right-0 rounded-full bg-purple-500 border-0 md:hover:bg-black cursor-pointer transition-colors"><img src="./assets/images/icon-arrow.svg" class="w-8 md:w-10 "></button>
+    <button class="calculate absolute grid place-items-center w-16 h-16 left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 md:left-[unset] md:translate-x-0 md:right-0 rounded-full bg-purple-500 border-0 md:hover:bg-black md:focus-visible:bg-black md:focus-visible:outline-0 md:active:scale-90 cursor-pointer transition-[background-color,scale]" aria-label="Button that allows you to calculate your age"><img src="./assets/images/icon-arrow.svg" class="w-8 md:w-10"></button>
     `;
 
 	return calculatorHead;
@@ -53,6 +53,7 @@ export const renderCalculator = () => {
 		"rounded-br-[25%]",
 		"md:mx-auto",
 		"md:px-8",
+		"shadow-md"
 	]);
 
 	calculator.append(renderCalculatorHead(), renderCalculatorBody());
@@ -60,7 +61,7 @@ export const renderCalculator = () => {
 	return calculator;
 };
 
-const giveElementClass = (element: Element, classes: string[]) => {
+export const giveElementClass = (element: Element, classes: string[]) => {
 	classes.forEach((cls) => {
 		element.classList.add(cls);
 	});
--- age-calculator-app-main/src/ts/script.ts
+++ age-calculator-app-main/src/ts/script.ts
@@ -1,5 +1,92 @@
 import { renderCalculator } from "./components.js";
+const currentYear = new Date().getFullYear();
+
+const addErrorState = (box: HTMLElement) => {
+	box.classList.add("error");
+};
+const removeErrorState = (box: HTMLElement) => {
+	box.classList.remove("error");
+};
+const showErrorTxt = (
+	txt: HTMLElement,
+	season: string,
+	isEmpty: boolean,
+	value: string
+) => {
+	txt.classList.remove("hidden");
+	if (isEmpty) {
+		txt.textContent = "This field is required";
+		return;
+	}
+
+	if (season === "year" && parseInt(value) > currentYear) {
+		txt.textContent = "Must be in the past";
+		return;
+	}
+
+	txt.textContent = `Must be a valid ${season}`;
+};
+const hideErrorTxt = (txt: HTMLElement) => {
+	txt.textContent = "";
+	txt.classList.add("hidden");
+};
+const handleInputs = () => {
+	const inputs = document.querySelectorAll<HTMLInputElement>(
+		'input[type="number"]'
+	);
+
+	if (inputs.length === 0) {
+		console.error("No input elements found");
+		return;
+	}
+
+	inputs.forEach((input) => {
+		const dayRegExp = /^(0[1-9]|[1-9]|[12][0-9]|3[01])$/;
+		const monthRegExp = /^(0?[1-9]|1[0-2])$/;
+		const yearRegExp = /^(19[0-9]{2}|20[0-1][0-9]|202[0-5])$/;
+
+		const value = input.value.trim();
+		const parent = input.closest<HTMLElement>(".group");
+		const errorTxt = input.nextElementSibling;
+		const season =
+			input.previousElementSibling?.textContent.toLowerCase() ?? "";
+		let isEmpty = false;
+		let isValid = false;
+
+		if (!parent) return;
+		if (!(errorTxt instanceof HTMLElement)) return;
+
+		addErrorState(parent);
+
+		if (value === "") {
+			isEmpty = true;
+			showErrorTxt(errorTxt, season, isEmpty, value);
+			return;
+		}
+		switch (season) {
+			case "day":
+				isValid = dayRegExp.test(value);
+				break;
+			case "month":
+				isValid = monthRegExp.test(value);
+				break;
+			case "year":
+				isValid = yearRegExp.test(value);
+				break;
+		}
+
+		if (!isValid) {
+			showErrorTxt(errorTxt, season, isEmpty, value);
+			return;
+		}
+		removeErrorState(parent);
+		hideErrorTxt(errorTxt);
+	});
+};
 
 document.addEventListener("DOMContentLoaded", function () {
 	document.body.append(renderCalculator());
+	document
+		.querySelector<HTMLButtonElement>(".calculate")
+		?.addEventListener("click", handleInputs);
 });
