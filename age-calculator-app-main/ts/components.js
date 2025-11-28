0dffca4d061c6841e0fe0801dc9de4b9c7a4028c
Tomek <tomek.wojak796@gmail.com>
2025-11-28 12:58:51 +0100

    added alt to img

--- age-calculator-app-main/ts/components.js
+++ age-calculator-app-main/ts/components.js
@@ -1,16 +1,16 @@
 const renderCalculatorHead = () => {
-	const calculatorHead = document.createElement("div");
-	giveElementClass(calculatorHead, [
-		"relative",
-		"flex",
-		"gap-4",
-		"md:gap-6",
-		"pt-10",
-		"pb-15",
-		"border-b-2",
-		"border-[hsla(0,1%,44%,20%)]",
-	]);
-	calculatorHead.innerHTML = `
+    const calculatorHead = document.createElement("div");
+    giveElementClass(calculatorHead, [
+        "relative",
+        "flex",
+        "gap-4",
+        "md:gap-6",
+        "pt-10",
+        "pb-15",
+        "border-b-2",
+        "border-[hsla(0,1%,44%,20%)]",
+    ]);
+    calculatorHead.innerHTML = `
     <div class="w-full sm:w-[24%] group"><span class="font-bold text-sm tracking-[0.2rem] text-grey-500 group-[.error]:text-red-400">DAY</span>
     <input type="number" placeholder="DD" class="py-3 px-4 rounded-lg font-bold uppercase text-black text-lg md:text-[1.3rem] placeholder:text-grey-500 w-full border border-[hsla(0,1%,44%,50%)] focus:border-purple-500 focus:outline-0 group-[.error]:border-red-400 caret-purple-500 cursor-pointer hover:border-purple-500 transition-colors">
     <span class="error-txt error-txt-main hidden italic text-red-400 font-medium text-xs md:text-sm"></span></div>
@@ -23,46 +23,38 @@ const renderCalculatorHead = () => {
     <input type="number" placeholder="YYYY" class="py-3 px-4 rounded-lg font-bold uppercase text-black text-lg md:text-[1.3rem] placeholder:text-grey-500 w-full border border-[hsla(0,1%,44%,50%)] focus:border-purple-500 focus:outline-0 group-[.error]:border-red-400 caret-purple-500 cursor-pointer hover:border-purple-500 transition-colors">
     <span class="error-txt hidden italic text-red-400 font-medium text-xs md:text-sm"></span></div>
 
-    <button class="calculate absolute grid place-items-center w-16 h-16 left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 md:left-[unset] md:translate-x-0 md:right-0 rounded-full bg-purple-500 border-0 md:hover:bg-black md:focus-visible:bg-black md:focus-visible:outline-0 md:active:scale-90 cursor-pointer transition-[background-color,scale]" aria-label="Button that allows you to calculate your age"><img src="./assets/images/icon-arrow.svg" alt="" class="w-8 md:w-10"></button>
+    <button class="calculate absolute grid place-items-center w-16 h-16 left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 md:left-[unset] md:translate-x-0 md:right-0 rounded-full bg-purple-500 border-0 md:hover:bg-black md:focus-visible:bg-black md:focus-visible:outline-0 md:active:scale-90 cursor-pointer transition-[background-color,scale]" aria-label="Button that allows you to calculate your age"><img src="./assets/images/icon-arrow.svg" class="w-8 md:w-10"></button>
     `;
-
-	return calculatorHead;
+    return calculatorHead;
 };
-
 const renderCalculatorBody = () => {
-	const calculatorBody = document.createElement("div");
-	giveElementClass(calculatorBody, ["pb-10", "pt-15"]);
-
-	calculatorBody.innerHTML = `
+    const calculatorBody = document.createElement("div");
+    giveElementClass(calculatorBody, ["pb-10", "pt-15"]);
+    calculatorBody.innerHTML = `
     <p class="font-black text-5xl sm:text-6xl md:text-7xl lg:text-[5.5rem] mb-2"><span class="dash inline-block align-middle -skew-x-15 w-5 h-2 sm:w-7 sm:h-3 bg-purple-500 "></span><span class="dash inline-block align-middle -skew-x-15 w-5 h-2 sm:w-7 sm:h-3 bg-purple-500 ml-3"></span><span class="info-year text-purple-500"></span> years</p>
     <p class="font-black text-5xl sm:text-6xl md:text-7xl lg:text-[5.5rem] mb-2"><span class="dash inline-block align-middle -skew-x-15 w-5 h-2 sm:w-7 sm:h-3 bg-purple-500  "></span><span class="dash inline-block align-middle -skew-x-15 w-5 h-2 sm:w-7 sm:h-3 bg-purple-500 ml-3"></span><span class="info-month text-purple-500"></span> months</p>
     <p class="font-black text-5xl sm:text-6xl md:text-7xl lg:text-[5.5rem] mb-2"><span class="dash inline-block align-middle -skew-x-15 w-5 h-2 sm:w-7 sm:h-3 bg-purple-500  "></span><span class="dash inline-block align-middle -skew-x-15 w-5 h-2 sm:w-7 sm:h-3 bg-purple-500 ml-3"></span><span class="info-day text-purple-500"></span> days</p>
     `;
-
-	return calculatorBody;
+    return calculatorBody;
 };
-
 export const renderCalculator = () => {
-	const calculator = document.createElement("div");
-	giveElementClass(calculator, [
-		"max-w-2xl",
-		"mx-3",
-		"px-5",
-		"bg-white",
-		"rounded-2xl",
-		"rounded-br-[25%]",
-		"md:mx-auto",
-		"md:px-8",
-		"shadow-md",
-	]);
-
-	calculator.append(renderCalculatorHead(), renderCalculatorBody());
-
-	return calculator;
+    const calculator = document.createElement("div");
+    giveElementClass(calculator, [
+        "max-w-2xl",
+        "mx-3",
+        "px-5",
+        "bg-white",
+        "rounded-2xl",
+        "rounded-br-[25%]",
+        "md:mx-auto",
+        "md:px-8",
+        "shadow-md",
+    ]);
+    calculator.append(renderCalculatorHead(), renderCalculatorBody());
+    return calculator;
 };
-
-export const giveElementClass = (element: Element, classes: string[]) => {
-	classes.forEach((cls) => {
-		element.classList.add(cls);
-	});
+export const giveElementClass = (element, classes) => {
+    classes.forEach((cls) => {
+        element.classList.add(cls);
+    });
 };
---
+++ age-calculator-app-main/ts/script.js
@@ -0,0 +1,176 @@
+import { renderCalculator } from "./components.js";
+const currentYear = new Date().getFullYear();
+const addErrorState = (box) => {
+    box.classList.add("error");
+};
+const removeErrorState = (box) => {
+    box.classList.remove("error");
+};
+const showErrorTxt = (txt, season, isEmpty, value, isShorter) => {
+    txt.classList.remove("hidden");
+    if (isEmpty) {
+        txt.textContent = "This field is required";
+        return;
+    }
+    if (season === "year" && parseInt(value) > currentYear) {
+        txt.textContent = "Must be in the past";
+        return;
+    }
+    if (isShorter) {
+        txt.textContent = "Must be a valid date";
+        return;
+    }
+    txt.textContent = `Must be a valid ${season}`;
+};
+const hideErrorTxt = (txt) => {
+    txt.textContent = "";
+    txt.classList.add("hidden");
+};
+const handleInputs = () => {
+    const inputs = document.querySelectorAll('input[type="number"]');
+    if (inputs.length === 0) {
+        console.error("No input elements found");
+        return;
+    }
+    inputs.forEach((input, index, inputs) => {
+        const dayRegExp = /^(0[1-9]|[1-9]|[12][0-9]|3[01])$/;
+        const monthRegExp = /^(0?[1-9]|1[0-2])$/;
+        const yearRegExp = /^(19[0-9]{2}|20[0-1][0-9]|202[0-5])$/;
+        const value = input.value.trim();
+        const parent = input.closest(".group");
+        const errorTxt = input.nextElementSibling;
+        const season = input.previousElementSibling?.textContent.toLowerCase() ?? "";
+        let isEmpty = false;
+        let isValid = false;
+        let isShorter = false;
+        if (!parent)
+            return;
+        if (!(errorTxt instanceof HTMLElement))
+            return;
+        addErrorState(parent);
+        if (value === "") {
+            isEmpty = true;
+            showErrorTxt(errorTxt, season, isEmpty, value, false);
+            return;
+        }
+        switch (season) {
+            case "day":
+                isValid = dayRegExp.test(value);
+                break;
+            case "month":
+                isValid = monthRegExp.test(value);
+                isShorter = checkIfShorter(value, [...inputs]);
+                const mainErrorTxt = document.querySelector(".error-txt-main");
+                if (!mainErrorTxt)
+                    return;
+                if (isShorter && isValid) {
+                    showErrorTxt(mainErrorTxt, season, isEmpty, value, isShorter);
+                }
+                break;
+            case "year":
+                isValid = yearRegExp.test(value);
+                break;
+        }
+        if (!isValid) {
+            showErrorTxt(errorTxt, season, isEmpty, value, false);
+            return;
+        }
+        removeErrorState(parent);
+        hideErrorTxt(errorTxt);
+    });
+    checkForErrors();
+};
+const checkForErrors = () => {
+    const boxes = [...document.querySelectorAll(".group")];
+    if (boxes.length === 0)
+        return;
+    const errorBoxes = boxes.filter((box) => box.classList.contains("error"));
+    if (errorBoxes.length !== 0)
+        return;
+    calculateAge();
+};
+const calculateAge = () => {
+    const allDashes = document.querySelectorAll(".dash");
+    const infoDay = document.querySelector(".info-day");
+    const infoMonth = document.querySelector(".info-month");
+    const infoYear = document.querySelector(".info-year");
+    const dayInput = document.querySelector('input[placeholder="DD"]');
+    const monthInput = document.querySelector('input[placeholder="MM"]');
+    const yearInput = document.querySelector('input[placeholder="YYYY"]');
+    if (!dayInput ||
+        !monthInput ||
+        !yearInput ||
+        !infoDay ||
+        !infoMonth ||
+        !infoYear)
+        return;
+    const day = Number(dayInput.value);
+    const month = Number(monthInput.value);
+    const year = Number(yearInput.value);
+    const birthDate = new Date(year, month - 1, day);
+    const currDate = new Date();
+    let years = currDate.getFullYear() - birthDate.getFullYear();
+    let months = currDate.getMonth() - birthDate.getMonth();
+    let days = currDate.getDate() - birthDate.getDate();
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+    if (days < 0) {
+        months--;
+        const lastMonth = new Date(currDate.getFullYear(), currDate.getMonth(), 0);
+        days += lastMonth.getDate();
+        if (months < 0) {
+            years--;
+            months += 12;
+        }
+    }
+    allDashes?.forEach((dash) => dash.remove());
+    infoDay.textContent = `${days}`;
+    infoMonth.textContent = `${months}`;
+    infoYear.textContent = `${years}`;
+};
+const checkIfShorter = (month, inputs) => {
+    const dayInputValue = inputs.find((input) => input.placeholder === "DD")?.value;
+    const yearInputValue = inputs.find((input) => input.placeholder === "YYYY")?.value;
+    if (!dayInputValue || !yearInputValue)
+        return false;
+    const allBoxes = document.querySelectorAll(".group");
+    let maxDate = 30;
+    let isLeap = checkIfLeapYear(yearInputValue);
+    let daysOfFeb = isLeap ? 29 : 28;
+    if ((month === "2" || month === "02") &&
+        parseInt(dayInputValue) > daysOfFeb) {
+        allBoxes.forEach((box) => box.classList.add("error"));
+        return true;
+    }
+    if ((month === "4" ||
+        month === "04" ||
+        month === "6" ||
+        month === "06" ||
+        month === "9" ||
+        month === "09" ||
+        month === "11") &&
+        parseInt(dayInputValue) > maxDate) {
+        allBoxes.forEach((box) => box.classList.add("error"));
+        return true;
+    }
+    return false;
+};
+const checkIfLeapYear = (year) => {
+    const parsedYear = parseInt(year);
+    return ((parsedYear % 4 == 0 && parsedYear % 100 != 0) || parsedYear % 400 == 0);
+};
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.append(renderCalculator());
+    document
+        .querySelector(".calculate")
+        ?.addEventListener("click", handleInputs);
+    document
+        .querySelectorAll('input[type="number"]')
+        .forEach((input) => input.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            handleInputs();
+        }
+    }));
+});
